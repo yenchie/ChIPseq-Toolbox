6be38a27a7a9947b490f6cb518f86176e288cf72
yenchie <ching.wish888@gmail.com>
2024-05-13 16:48:35 +0800

    fixed GO_enrichment bug, and tested. remain fix: wrong cat number in DAG

--- GO_enrichment/GOseg.fun/GOseq.hyper.FDR.DEG.run.20230914.R
+++ GO_enrichment/GOseg.fun/GOseq.hyper.FDR.DEG.run.20230914.R
@@ -1,4 +1,4 @@
-source("/nas/cluster_data/homes/yenching/JYL/JYL_qnap_2/YCWang/0_Script/000/Toolbox/GO_enrichment/GOseq.hyper.FDR.DEG.fun.v.2.R")
+source("/nas/cluster_data/homes/yenching/JYL/JYL_qnap_2/YCWang/0_Script/000/Toolbox/GO_enrichment/GOseg.fun/GOseq.hyper.FDR.DEG.fun.v.2.R")
 # datapath<-"/bcst/JYL/JYL_qnap_2/YCWang/Project/Gm/RNAseq/Gm_seed_develop/GO_enrichment/AdjUp_mm_306TF_id"
 datapath <- "/homes/yenching/testing/GO_enrichment/RO_test.Data"
 # name="/bcst/JYL/JYL_qnap_2/YCWang/Project/Gm/RNAseq/Gm_seed_develop/GO_enrichment/adjUP/output/GOterm/adjUp_sdlg_GO0006355_regulation_of_transcription.TF_GOseq.enrichment.txt"
